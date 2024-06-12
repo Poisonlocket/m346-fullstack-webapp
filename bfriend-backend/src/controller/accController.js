6b174093ccab9a30d87af96781a0936f9b8302a0
kollert-bzz <kollert@bzz.ch>
2024-06-12 13:53:46 +0200

    ...

--- bfriend-backend/src/controller/accController.js
+++ bfriend-backend/src/controller/accController.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const {Client} = require('pg');
+const { Client } = require('pg');
 require("dotenv").config();
-const admin_password = process.env.ADMIN_PW;
 
-
+const adminPassword = process.env.ADMIN_PW;
 const router = express.Router();
 
 const client = new Client({
@@ -25,7 +24,6 @@ client.connect(err => {
     }
 });
 
-
 const isAuthenticated = (req, res, next) => {
     if (!req.session || req.session.auth !== 'auth') {
         return res.status(401).send('Not Logged in');
@@ -44,83 +42,108 @@ const isAdmin = async (client, username) => {
     return userData.is_admin;
 };
 
+const isActive = async (client, username) => {
+    const query = "SELECT active_profile FROM user_data WHERE username = $1";
+    const values = [username];
+    const result = await client.query(query, values);
+    const userData = result.rows[0];
+    if (!userData) {
+        throw new Error('User not found');
+    }
+    return userData.active_profile;
+};
 
-router.get("/api/users", isAuthenticated, async (req, res) => {
+const isAdminAndActive = async (req, res, next) => {
     try {
         const username = req.session.user;
         const userIsAdmin = await isAdmin(client, username);
+        const userIsActive = await isActive(client, username);
 
-        if (userIsAdmin) {
-            const allUsers = await client.query('SELECT * FROM user_data;');
-            return res.json(allUsers.rows);
-        } else {
+        if (!userIsAdmin) {
             return res.status(403).send('Access denied. Admins only.');
         }
+
+        if (!userIsActive) {
+            return res.status(403).send("Your profile isn't active.");
+        }
+
+        next();
     } catch (err) {
         if (err.message === "User not found") {
             return res.status(404).send("User not found.");
         }
         res.status(500).send("Database query error");
     }
-});
+};
 
-
-router.get("/api/users/:id", isAuthenticated, async (req, res) => {
+router.get("/api/users", isAuthenticated, isAdminAndActive, async (req, res) => {
     try {
-        const username = req.session.user;
-        const userIsAdmin = await isAdmin(client, username);
-
-        if (userIsAdmin) {
-            const query = 'SELECT * FROM user_data WHERE id = $1';
-            const values = [req.params.id];
-            const result = await client.query(query, values);
-            return res.json(result.rows);
-        } else {
-            return res.status(403).send('Access denied. Admins only.');
-        }
+        const allUsers = await client.query('SELECT * FROM user_data;');
+        res.json(allUsers.rows);
     } catch (err) {
         res.status(500).send("Database query error");
     }
 });
 
+router.get("/api/users/:id", isAuthenticated, isAdminAndActive, async (req, res) => {
+    try {
+        const query = 'SELECT * FROM user_data WHERE id = $1';
+        const values = [req.params.id];
+        const result = await client.query(query, values);
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).send("Database query error");
+    }
+});
 
 router.get("/api/my-data", isAuthenticated, async (req, res) => {
     try {
         const query = 'SELECT * FROM user_data WHERE username = $1';
         const values = [req.session.user];
         const result = await client.query(query, values);
-        return res.json(result.rows);
+        res.json(result.rows);
     } catch (err) {
         res.status(500).send("Database query error");
     }
 });
-
 
 router.put("/api/deactivate", isAuthenticated, async (req, res) => {
     try {
-        const query = 'UPDATE user_data SET active_profile = $1 WHERE username = $2';
-        const values = [false, req.session.user];
-        await client.query(query, values);
-        res.send();
-    } catch (err) {
+        const username = req.session.user;
+        const userIsActive = await isActive(client, username);
+
+        if (userIsActive) {
+            const query = 'UPDATE user_data SET active_profile = $1 WHERE username = $2';
+            const values = [false, req.session.user];
+            await client.query(query, values);
+            res.send('Profile Deactivated');
+    } else{
+            res.send('Profile already Deactivated');
+    }}
+    catch (err) {
         res.status(500).send("Database query error");
     }
 });
-
 
 router.put("/api/activate", isAuthenticated, async (req, res) => {
     try {
-        const query = 'UPDATE user_data SET active_profile = $1 WHERE username = $2';
-        const values = [true, req.session.user];
-        await client.query(query, values);
-        res.send();
-    } catch (err) {
+        const username = req.session.user;
+        const userIsActive = await isActive(client, username);
+
+        if (!userIsActive) {
+            const query = 'UPDATE user_data SET active_profile = $1 WHERE username = $2';
+            const values = [true, req.session.user];
+            await client.query(query, values);
+            res.send('Profile Activated');
+        } else{
+            res.send('Profile already Activated');
+        }}
+    catch (err) {
         res.status(500).send("Database query error");
     }
 });
 
-
-router.delete("/api/delete", async (req, res) => {
+router.delete("/api/delete", isAuthenticated, async (req, res) => {
     try {
         const query = 'DELETE FROM user_data WHERE username = $1';
         const values = [req.session.user];
@@ -131,19 +154,18 @@ router.delete("/api/delete", async (req, res) => {
     }
 });
 
-
 router.put("/api/get-admin", isAuthenticated, async (req, res) => {
     try {
         const username = req.session.user;
         const userIsAdmin = await isAdmin(client, username);
+
         if (userIsAdmin) {
             res.send('Du bist schon ein Administrator');
-        }
-        else if (req.body.apw === admin_password) {
+        } else if (req.body.apw === adminPassword) {
             const query = 'UPDATE user_data SET is_admin = $1 WHERE username = $2';
             const values = [true, req.session.user];
             await client.query(query, values);
-            res.send('Du Bist jetzt ein Administrator');
+            res.send('Du bist jetzt ein Administrator');
         } else {
             res.send("Das Passwort ist falsch!");
         }
